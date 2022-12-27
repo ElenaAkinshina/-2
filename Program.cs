e297537eb31479084ee984916ab4cd1e05fdd600
елена <el.bukreeva48 @yandex.ru>
2022-12-27 10:41:56 +0300

    Строки2

--- Program.cs
+++ Program.cs
@@ -59,6 +59,20 @@ if ((number > 9999) && (number < 100000)){
     Console.WriteLine($"{number} -> {num_2}");
     }*/
 
+    /*Console.WriteLine("Строково:");
+    Console.Write("Введите число: ");
+    int numb2 = int.Parse(Console.ReadLine()!);
+    string numb_2 = numb2.ToString();
+    if(numb_2.Length > 5){
+        Console.WriteLine($"Число введено неверно");
+    }
+   else if(numb_2.Length > 3) {
+        Console.WriteLine($"Третьей цифры нет");
+   }
+    else{
+        Console.WriteLine($"Третья цифра числа {numb2} -> {numb_2[2]");
+    }
+*/
     /*Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. Обязательна проверка на ввод числа <1 и >7
 
 6 -> да
@@ -67,7 +81,7 @@ if ((number > 9999) && (number < 100000)){
 
 Console.WriteLine("Введите число от 1 до 7: ");
 int num = int.Parse(Console.ReadLine()!);
-if ((num < 1) && (num > 7)){
+if ((num < 1) || (num > 7)){
      Console.WriteLine($"{num} -> не подходит");
 }
 if ((num > 0) && (num < 6)){
