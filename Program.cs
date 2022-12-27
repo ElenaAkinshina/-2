a0f9ff295250496429c9c030945bdb3903f2bc83
елена <el.bukreeva48 @yandex.ru>
2022-12-27 10:54:16 +0300

    Строки3

--- Program.cs
+++ Program.cs
@@ -59,27 +59,27 @@ if ((number > 9999) && (number < 100000)){
     Console.WriteLine($"{number} -> {num_2}");
     }*/
 
-    /*Console.WriteLine("Строково:");
-    Console.Write("Введите число: ");
-    int numb2 = int.Parse(Console.ReadLine()!);
-    string numb_2 = numb2.ToString();
-    if(numb_2.Length > 5){
-        Console.WriteLine($"Число введено неверно");
-    }
-   else if(numb_2.Length > 3) {
-        Console.WriteLine($"Третьей цифры нет");
-   }
-    else{
-        Console.WriteLine($"Третья цифра числа {numb2} -> {numb_2[2]");
-    }
+/*Console.WriteLine("Строково:");
+Console.Write("Введите число: ");
+int numb2 = int.Parse(Console.ReadLine()!);
+string numb_2 = numb2.ToString();
+if(numb_2.Length > 5){
+    Console.WriteLine($"Число введено неверно");
+}
+else if(numb_2.Length > 3) {
+    Console.WriteLine($"Третьей цифры нет");
+}
+else{
+    Console.WriteLine($"Третья цифра числа {numb2} -> {numb_2[2]");
+}
 */
-    /*Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. Обязательна проверка на ввод числа <1 и >7
+/*Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. Обязательна проверка на ввод числа <1 и >7
 
 6 -> да
 7 -> да
 1 -> нет*/
 
-Console.WriteLine("Введите число от 1 до 7: ");
+/*Console.WriteLine("Введите число от 1 до 7: ");
 int num = int.Parse(Console.ReadLine()!);
 if ((num < 1) || (num > 7)){
      Console.WriteLine($"{num} -> не подходит");
@@ -89,5 +89,58 @@ if ((num > 0) && (num < 6)){
     }
 if ((num > 5) && (num < 8)){
     Console.WriteLine($"{num} -> да");
-    }
+    }*/
 
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число от 1 до 7: ");
+        int num = int.Parse(Console.ReadLine()!);
+        switch (num)
+        {
+            case 1:
+                {
+                    Console.WriteLine("Это понедельник");
+                    break;
+                }
+            case 2:
+                {
+                    Console.WriteLine("Это вторник");
+                    break;
+                }
+            case 3:
+                {
+                    Console.WriteLine("Это среда");
+                    break;
+                }
+            case 4:
+                {
+                    Console.WriteLine("Это четверг");
+                    break;
+                }
+            case 5:
+                {
+                    Console.WriteLine("Это пятница");
+                    break;
+                }    
+            case 6:
+                {
+                    Console.WriteLine("Это суббота");
+                    break;
+                }
+            case 7:
+                {
+                    Console.WriteLine("Это воскресение");
+                    break;
+                }
+
+            default :
+                {
+                    Console.WriteLine("Ошибка");
+                    break;
+                }
+        }
+    }
+}
